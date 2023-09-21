166447d9fa5a0a6e23e65a0220daaa9bbc591aea
Cleyton <cleytontyukrs62@gmail.com>
2023-09-21 17:36:46 -0300

    criado atributos de dados

--- oo/pessao.py
+++ oo/pessao.py
@@ -1,6 +1,11 @@
 class Pessoa:
-   def cumprimentar(self):
+    def __init__(self, nome='Cleyton', idade=30):
+        self.idade = idade
+        self.nome = nome
+    def cumprimentar(self):
        return 'Ola'
 if __name__ == '__main__':
     p = Pessoa()
     print(p.cumprimentar())
+    print(p.idade)
+    print(p.nome)
